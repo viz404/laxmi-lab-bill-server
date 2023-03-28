dd97869ace5653b1c95c3aba3fa6a3dfa6b32c5b
vishal <vishal.gxm@gmail.com>
2023-03-28 22:16:05 +0530

    added all crud routes

--- src/routes/job.route.js
+++ src/routes/job.route.js
@@ -1,8 +1,18 @@
 const { Router } = require("express");
-const { addJob } = require("../controllers/job.controller");
+const {
+  addJob,
+  getJobs,
+  getJobById,
+  updateJobById,
+  deleteJobById,
+} = require("../controllers/job.controller");
 
 const jobRouter = Router();
 
 jobRouter.post("/", addJob);
+jobRouter.get("/", getJobs);
+jobRouter.get("/:id", getJobById);
+jobRouter.patch("/:id", updateJobById);
+jobRouter.delete("/:id", deleteJobById);
 
 module.exports = jobRouter;
