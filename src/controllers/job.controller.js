5338745d94a44f3c2f7c7d76b15f26a4a5022c8e
vishal <vishal.gxm@gmail.com>
2023-03-28 22:15:50 +0530

    created get, getById, updateById and deleteById

--- src/controllers/job.controller.js
+++ src/controllers/job.controller.js
@@ -1,4 +1,5 @@
 const JobModel = require("../models/job.model");
+const countDocuments = require("../helper/countDocuments");
 
 const addJob = async (req, res) => {
   try {
@@ -13,4 +14,85 @@ const addJob = async (req, res) => {
   }
 };
 
-module.exports = { addJob };
+const getJobs = async (req, res) => {
+  try {
+    const {
+      doctor_id,
+      from_date,
+      till_date,
+      _limit = 10,
+      _page = 1,
+    } = req.query;
+
+    const skip = _page > 0 ? (_page - 1) * _limit : 0;
+
+    let filters = {};
+
+    if (doctor_id) {
+      filters.doctorId = doctor_id;
+    }
+
+    if (from_date && till_date) {
+      let fromDate = new Date(from_date);
+      let tillDate = new Date(till_date);
+
+      filters.date = { $gte: fromDate, $lte: tillDate };
+    }
+
+    const response = await JobModel.find(filters)
+      .sort({ date: -1 })
+      .skip(skip)
+      .limit(_limit);
+
+    const count = await countDocuments(JobModel);
+
+    res.set("X-Total-Count", count);
+
+    return res.json({ response, status: true });
+  } catch (error) {
+    res.status(500);
+    return res.json({ error: error.message, status: false });
+  }
+};
+
+const getJobById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const response = await JobModel.findById(id);
+
+    return res.json({ response, status: true });
+  } catch (error) {
+    res.status(500);
+    return res.json({ error: error.message, status: false });
+  }
+};
+
+const updateJobById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedJob = req.body;
+
+    const response = await JobModel.findByIdAndUpdate(id, updatedJob);
+
+    return res.json({ response, status: true });
+  } catch (error) {
+    res.status(500);
+    return res.json({ error: error.message, status: false });
+  }
+};
+
+const deleteJobById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const response = await JobModel.findByIdAndDelete(id);
+
+    return res.json({ response, status: true });
+  } catch (error) {
+    res.status(500);
+    return res.json({ error: error.message, status: false });
+  }
+};
+
+module.exports = { addJob, getJobs, getJobById, updateJobById, deleteJobById };
