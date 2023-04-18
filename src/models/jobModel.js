ad042d6c91211c5f35fb1d455fdbcbfa67fed068
vishal <vishal.gxm@gmail.com>
2023-04-18 15:14:03 +0530

    added id, doctor and timestamps field to model

--- src/models/jobModel.js
+++ src/models/jobModel.js
@@ -1,25 +1,28 @@
 const { Schema, model } = require("mongoose");
 
-const jobSchema = new Schema({
-  date: { type: Date, required: true },
-  doctorId: { type: Schema.Types.ObjectId, required: true, ref: "Doctor" },
-  doctorName: { type: String, required: true },
-  jobNumber: { type: Number, required: true },
-  patientName: { type: String, required: true },
-  shade: String,
-  notes: String,
-  price: { type: Number, required: true },
-  works: [
-    {
-      title: { type: String, required: true },
-      singlePrice: { type: Number, required: true },
-      topLeft: String,
-      topRight: String,
-      bottomLeft: String,
-      bottomRight: String,
-    },
-  ],
-});
+const jobSchema = new Schema(
+  {
+    _id: { type: Number, required: true },
+    doctor: { type: Schema.Types.ObjectId, required: true, ref: "Doctor" },
+    date: { type: Date, required: true },
+    jobNumber: { type: Number, required: true },
+    patientName: { type: String, required: true },
+    shade: String,
+    notes: String,
+    price: { type: Number, required: true },
+    works: [
+      {
+        title: { type: String, required: true },
+        singlePrice: { type: Number, required: true },
+        topLeft: String,
+        topRight: String,
+        bottomLeft: String,
+        bottomRight: String,
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 const JobModel = model("Job", jobSchema);
 
