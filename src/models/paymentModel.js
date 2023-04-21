8a707ff0be7d5441da02c11d13bcdaec22596651
vishal <vishal.gxm@gmail.com>
2023-04-22 00:29:57 +0530

    created payment model

---
+++ src/models/paymentModel.js
@@ -0,0 +1,15 @@
+const { Schema, model } = require("mongoose");
+
+const paymentSchema = new Schema({
+  _id: { type: Number, required: true },
+  doctor: { type: Number, ref: "Doctor", required: true },
+  amount: { type: Number, required: true },
+  date: { type: Date, required: true },
+  mode: { type: String, required: true },
+  mobile: Number,
+  chequeNumber: Number,
+});
+
+const PaymentModel = model("Payment", paymentSchema);
+
+module.exports = PaymentModel;
