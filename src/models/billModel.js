4d1f8f9787ea553c426eb74127fc4ebcafb089c4
vishal <vishal.gxm@gmail.com>
2023-04-21 14:56:32 +0530

    changed reference type from object id to number

--- src/models/billModel.js
+++ src/models/billModel.js
@@ -3,11 +3,11 @@ const { Schema, model } = require("mongoose");
 const billSchema = new Schema(
   {
     _id: { type: Number, required: true },
-    doctor: { type: Schema.Types.ObjectId, ref: "Doctor", required: true },
+    doctor: { type: Number, ref: "Doctor", required: true },
     fromDate: { type: Date, required: true },
     tillDate: { type: Date, required: true },
     totalAmount: { type: Number, required: true },
-    jobs: [{ type: Schema.Types.ObjectId, ref: "Job", required: true }],
+    jobs: [{ type: Number, ref: "Job", required: true }],
   },
   { timestamps: true }
 );
