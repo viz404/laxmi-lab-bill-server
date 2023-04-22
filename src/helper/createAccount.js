ad3ff629a5a5bd4941e6f23f2489542b8ea8b4a9
vishal <vishal.gxm@gmail.com>
2023-04-22 21:11:29 +0530

    updated function to add doctor name

--- src/helper/createAccount.js
+++ src/helper/createAccount.js
@@ -2,13 +2,18 @@ const AccountModel = require("../models/accountModel");
 
 const incrementCount = require("./incrementCount");
 
-const createAccount = async (doctor_id) => {
+const createAccount = async (doctor_id, doctor_name) => {
   if (!doctor_id) {
     throw new Error("no doctor id recieved");
   }
 
   const _id = await incrementCount("account_id");
-  await AccountModel.create({ _id, doctor: doctor_id, balance: 0 });
+  await AccountModel.create({
+    _id,
+    doctor: doctor_id,
+    doctorName: doctor_name,
+    balance: 0,
+  });
 };
 
 module.exports = createAccount;
