a81d6b4e7e31feb70aa2ebef1ba33037b1a9fdbe
vishal <vishal.gxm@gmail.com>
2023-04-22 21:40:55 +0530

    created get transactions controller

---
+++ src/controllers/transactionController.js
@@ -0,0 +1,29 @@
+const TransactionModel = require("../models/transactionModel");
+
+const getTransactions = async (req, res) => {
+  try {
+    const { doctor_id, from_date, till_date } = req.query;
+
+    if (!doctor_id || !from_date || !till_date) {
+      res.status(400);
+      return res.json({ error: "Incomplete details", status: false });
+    }
+
+    let filters = { doctor: doctor_id };
+
+    let fromDate = new Date(from_date);
+    let tillDate = new Date(till_date);
+
+    filters.createdAt = { $gte: fromDate, $lte: tillDate };
+
+    const response = TransactionModel.find(filters).populate("doctor");
+
+    return res.json({ response, status: true });
+  } catch (error) {
+    console.log(error);
+    res.status(500);
+    return res.json({ error: error.message, status: false });
+  }
+};
+
+module.exports = { getTransactions };
