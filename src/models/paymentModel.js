54894455016e64d35981d044d720aa42075513e0
vishal <vishal.gxm@gmail.com>
2023-04-23 16:33:15 +0530

    added doctorName field

--- src/models/paymentModel.js
+++ src/models/paymentModel.js
@@ -3,6 +3,7 @@ const { Schema, model } = require("mongoose");
 const paymentSchema = new Schema({
   _id: { type: Number, required: true },
   doctor: { type: Number, ref: "Doctor", required: true },
+  doctorName: { type: String, required: true },
   amount: { type: Number, required: true },
   date: { type: Date, required: true },
   mode: { type: String, required: true },
