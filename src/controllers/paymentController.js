06c47e698fac62511b7097256619076e5b5239f7
vishal <vishal.gxm@gmail.com>
2023-04-23 16:33:11 +0530

    created get payments controller

--- src/controllers/paymentController.js
+++ src/controllers/paymentController.js
@@ -3,6 +3,7 @@ const PaymentModel = require("../models/paymentModel");
 const incrementCount = require("../helper/incrementCount");
 const updateAccountBalance = require("../helper/updateAccountBalance");
 const createTransaction = require("../helper/createTransaction");
+const countDocuments = require("../helper/countDocuments");
 
 const addPayment = async (req, res) => {
   try {
@@ -34,4 +35,33 @@ const addPayment = async (req, res) => {
   }
 };
 
-module.exports = { addPayment };
+const getPayments = async (req, res) => {
+  try {
+    const { _limit = 10, _page = 1, doctor_name } = req.query;
+    const skip = _page > 0 ? (_page - 1) * _limit : 0;
+
+    let filters = {};
+
+    if (doctor_name) {
+      filters.doctorName = { $regex: new RegExp(doctor_name, "i") };
+    }
+
+    const response = await PaymentModel.find(filters)
+      .skip(skip)
+      .limit(_limit)
+      .sort({ date: -1 })
+      .populate("doctor");
+
+    const count = await countDocuments(PaymentModel);
+
+    res.set("X-Total-Count", count);
+
+    return res.json({ response, status: true });
+  } catch (error) {
+    console.log(error);
+    res.status(500);
+    return res.json({ error: error.message, status: false });
+  }
+};
+
+module.exports = { addPayment, getPayments };
