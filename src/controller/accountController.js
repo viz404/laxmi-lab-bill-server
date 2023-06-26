ab75a442916b49c49dcf74f10e71b2eaab2b233b
viz404 <vishal.gxm@gmail.com>
2023-06-27 02:48:48 +0530

    get account halfway

--- src/controller/accountController.js
+++ src/controller/accountController.js
@@ -18,3 +18,39 @@ async function updateAccount(req, res) {
     res.status(500).json({ status: false, error: "Something went wrong" });
   }
 }
+
+async function getAccount(req, res) {
+  try {
+    const doctorId = req.params.doctorId;
+
+    const response = await AccountModel.aggregate([
+      {
+        $match: {"doctor.id": doctorId}
+      },
+      {
+        $lookup: {
+          from: "doctors",
+          localField: "doctor.id",
+          foreignField: "id",
+          as: "doctor_details",
+        }
+      },
+      {
+        $project: {
+          _id: 0,
+          __v: 0,
+          "doctor_details._id": 0,
+          "doctor_details.__v": 0,
+          "doctor_details.works": 0,
+        }
+      },
+      {
+        $unwind: "$doctor_details",
+      }
+    ]);
+    // add lookup for bill and payment
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ status: false, error: "Something went wrong" });
+  }
+}
